56cd3f34b6ead5489dafc78ec79cdb2e72508b52
CeF95 <celeste.fuentes.g30@gmail.com>
2026-04-15 23:29:22 -0600

    Se realiza la implementación de Eliminar al inicio.

--- src/ListaDobleCircular.java
+++ src/ListaDobleCircular.java
@@ -28,14 +28,27 @@ public class ListaDobleCircular {
         } else { // Si la lista NO está vacía, se inserta el nuevo nodo al final
             Nodo ultimo = head.anterior; // Obtener el último nodo de la lista (el nodo anterior a la cabeza)
             ultimo.siguiente = nuevoNodo; // El último nodo apunta al nuevo nodo como siguiente
-            nuevoNodo.anterior = ultimo; // El nuevo nodo apunta al último nodo como anterior
+            nuevoNodo.anterior = ultimo; // El  nuevo nodo apunta al último nodo como anterior
             nuevoNodo.siguiente = head; // El nuevo nodo apunta a la cabeza actual como siguiente
             head.anterior = nuevoNodo; // La cabeza actual apunta al nuevo nodo como anterior
         } //OJO aca no se actualiza la cabeza porque el nuevo nodo se inserta al final, no al inicio
     }
 
     // Eliminar al Inicio 
-    public void eliminarAlInicio() {}
+    public void eliminarAlInicio() { // Verificar si la lista está vacía
+        if (head == null) { // Si la lista está vacía, no se puede eliminar ningún nodo
+            System.out.println("La lista esta vacia nose puede eliminar");
+            return; // Salir del método sin hacer nada
+        }
+        if (head.siguiente == head) { // Si solo hay un nodo en la lista
+            head = null; // La lista queda vacía
+        } else { // Si hay más de un nodo en la lista
+            Nodo ultimo = head.anterior; // Obtener el último nodo de la lista (el head anterior)
+            head = head.siguiente; // El head de la lista ahora es el siguiente nodo
+            head.anterior = ultimo; // El nuevo head apunta al último nodo como anterior
+            ultimo.siguiente = head; // El último nodo apunta al nuevo head como siguiente
+        }
+    }
 
     // Eliminar al Final 
     public void eliminarAlFinal() {}
