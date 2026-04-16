567893180bb107f2690a694d379a3b22ad6785d7
CeF95 <celeste.fuentes.g30@gmail.com>
2026-04-15 23:04:59 -0600

    Se construye la opcion de insretar al inicio

--- src/ListaDobleCircular.java
+++ src/ListaDobleCircular.java
@@ -2,7 +2,22 @@ public class ListaDobleCircular {
     private Nodo head = null;
 
     // Insertar al Inicio 
-    public void insertarAlInicio() {}
+    public void insertarAlInicio(int dato) {
+        Nodo.nuevoNodo = new Nodo(dato) { // Crear un nuevo nodo con el dato proporcionado
+        if (head == null) { // Si la lista está vacía, el nuevo nodo será la cabeza
+            nuevoNodo.siguiente = nuevoNodo; //Nuevo nodo apunta a sí mismo como siguiente
+            nuevoNodo.anterior = nuevoNodo; //Nuevo nodo apunta a sí mismo como anterior
+            head = nuevoNodo; // La cabeza de la lista ahora es el nuevo nodo
+        } else { // Si la lista NO está vacía, se inserta el nuevo nodo al inicio
+            Nodo ultimo = head.anterior; // Obtener el último nodo de la lista (el nodo anterior a la cabeza)
+            nuevoNodo.siguiente = head; // El nuevo nodo apunta a la cabeza actual como siguiente
+            nuevoNodo.anterior = ultimo; // El nuevo nodo apunta al último nodo como anterior
+            ultimo.siguiente = nuevoNodo; // El último nodo apunta al nuevo nodo como siguiente
+            head.anterior = nuevoNodo; // La cabeza actual apunta al nuevo nodo como anterior
+            head = nuevoNodo;  // La cabeza de la lista ahora es el nuevo nodo     
+        }
+        }
+    }
 
     // Insertar al Final 
     public void insertarAlFinal() {}
