27f463c1689dbcd95a7ae40fb608f5cf09414d10
CeF95 <celeste.fuentes.g30@gmail.com>
2026-04-15 23:10:56 -0600

    Se implementa la operación de INSERTAR AL FINAL

--- src/ListaDobleCircular.java
+++ src/ListaDobleCircular.java
@@ -16,11 +16,23 @@ public class ListaDobleCircular {
             head.anterior = nuevoNodo; // La cabeza actual apunta al nuevo nodo como anterior
             head = nuevoNodo;  // La cabeza de la lista ahora es el nuevo nodo     
         }
-        }
     }
 
     // Insertar al Final 
-    public void insertarAlFinal() {}
+    public void insertarAlFinal() {
+        Nodo nuevoNodo = new Nodo(dato); // Crear un nuevo nodo con el dato proporcionado
+        if (head == null) { // Si la lista está vacía, el nuevo nodo será la cabeza
+            nuevoNodo.siguiente = nuevoNodo; //Nuevo nodo apunta a sí mismo como siguiente
+            nuevoNodo.anterior = nuevoNodo; //Nuevo nodo apunta a sí mismo como anterior
+            head = nuevoNodo; // La cabeza de la lista ahora es el nuevo nodo
+        } else { // Si la lista NO está vacía, se inserta el nuevo nodo al final
+            Nodo ultimo = head.anterior; // Obtener el último nodo de la lista (el nodo anterior a la cabeza)
+            ultimo.siguiente = nuevoNodo; // El último nodo apunta al nuevo nodo como siguiente
+            nuevoNodo.anterior = ultimo; // El nuevo nodo apunta al último nodo como anterior
+            nuevoNodo.siguiente = head; // El nuevo nodo apunta a la cabeza actual como siguiente
+            head.anterior = nuevoNodo; // La cabeza actual apunta al nuevo nodo como anterior
+        } //OJO aca no se actualiza la cabeza porque el nuevo nodo se inserta al final, no al inicio
+    }
 
     // Eliminar al Inicio 
     public void eliminarAlInicio() {}
