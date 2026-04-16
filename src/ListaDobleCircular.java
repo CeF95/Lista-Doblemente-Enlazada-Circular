d3a36f5c2ca1158d26ac920a850bf756d7f4aaa9
CeF95 <celeste.fuentes.g30@gmail.com>
2026-04-15 23:40:39 -0600

    Se implementa el paso 6.4

--- src/ListaDobleCircular.java
+++ src/ListaDobleCircular.java
@@ -3,35 +3,35 @@ public class ListaDobleCircular {
 
     // Insertar al Inicio 
     public void insertarAlInicio(int dato) {
-        Nodo.nuevoNodo = new Nodo(dato) { // Crear un nuevo nodo con el dato proporcionado
-        if (head == null) { // Si la lista está vacía, el nuevo nodo será la cabeza
+        Nodo nuevoNodo = new Nodo(dato);  // Crear un nuevo nodo con el dato proporcionado
+        if (head == null) { // Si la lista está vacía, el nuevo nodo será el head
             nuevoNodo.siguiente = nuevoNodo; //Nuevo nodo apunta a sí mismo como siguiente
             nuevoNodo.anterior = nuevoNodo; //Nuevo nodo apunta a sí mismo como anterior
-            head = nuevoNodo; // La cabeza de la lista ahora es el nuevo nodo
+            head = nuevoNodo; // El head de la lista ahora es el nuevo nodo
         } else { // Si la lista NO está vacía, se inserta el nuevo nodo al inicio
-            Nodo ultimo = head.anterior; // Obtener el último nodo de la lista (el nodo anterior a la cabeza)
-            nuevoNodo.siguiente = head; // El nuevo nodo apunta a la cabeza actual como siguiente
+            Nodo ultimo = head.anterior; // Obtener el último nodo de la lista (el nodo headanterior)
+            nuevoNodo.siguiente = head; // El nuevo nodo apunta al head actual como siguiente
             nuevoNodo.anterior = ultimo; // El nuevo nodo apunta al último nodo como anterior
             ultimo.siguiente = nuevoNodo; // El último nodo apunta al nuevo nodo como siguiente
-            head.anterior = nuevoNodo; // La cabeza actual apunta al nuevo nodo como anterior
-            head = nuevoNodo;  // La cabeza de la lista ahora es el nuevo nodo     
+            head.anterior = nuevoNodo; // El head actual apunta al nuevo nodo como anterior
+            head = nuevoNodo;  // El head de la lista ahora es el nuevo nodo     
         }
     }
 
     // Insertar al Final 
-    public void insertarAlFinal() {
+    public void insertarAlFinal(int dato) {
         Nodo nuevoNodo = new Nodo(dato); // Crear un nuevo nodo con el dato proporcionado
-        if (head == null) { // Si la lista está vacía, el nuevo nodo será la cabeza
+        if (head == null) { // Si la lista está vacía, el nuevo nodo será el head
             nuevoNodo.siguiente = nuevoNodo; //Nuevo nodo apunta a sí mismo como siguiente
             nuevoNodo.anterior = nuevoNodo; //Nuevo nodo apunta a sí mismo como anterior
-            head = nuevoNodo; // La cabeza de la lista ahora es el nuevo nodo
+            head = nuevoNodo; // El head de la lista ahora es el nuevo nodo
         } else { // Si la lista NO está vacía, se inserta el nuevo nodo al final
-            Nodo ultimo = head.anterior; // Obtener el último nodo de la lista (el nodo anterior a la cabeza)
+            Nodo ultimo = head.anterior; // Obtener el último nodo de la lista (el nodo head anterior)
             ultimo.siguiente = nuevoNodo; // El último nodo apunta al nuevo nodo como siguiente
             nuevoNodo.anterior = ultimo; // El  nuevo nodo apunta al último nodo como anterior
-            nuevoNodo.siguiente = head; // El nuevo nodo apunta a la cabeza actual como siguiente
-            head.anterior = nuevoNodo; // La cabeza actual apunta al nuevo nodo como anterior
-        } //OJO aca no se actualiza la cabeza porque el nuevo nodo se inserta al final, no al inicio
+            nuevoNodo.siguiente = head; // El nuevo nodo apunta al head actual como siguiente
+            head.anterior = nuevoNodo; // El head actual apunta al nuevo nodo como anterior
+        } //OJO aca no se actualiza el head porque el nuevo nodo se inserta al final, no al inicio
     }
 
     // Eliminar al Inicio 
@@ -51,7 +51,19 @@ public class ListaDobleCircular {
     }
 
     // Eliminar al Final 
-    public void eliminarAlFinal() {}
+    public void eliminarAlFinal() { // Verificar si la lista está vacía
+        if (head == null) { // Si la lista está vacía, no se puede eliminar ningún nodo
+            System.out.println("La lista esta vacia nose puede eliminar");
+            return; // Salir del método sin hacer nada
+        }
+        if (head.siguiente == head) { // Si solo hay un nodo en la lista
+            head = null; // La lista queda vacía
+        } else { // Si hay más de un nodo en la lista
+            Nodo ultimo = head.anterior; // Obtener el último nodo de la lista (el head anterior)
+            ultimo.anterior.siguiente = head; // El nodo anterior al último apunta al head como siguiente
+            head.anterior = ultimo.anterior; // El head apunta al nodo anterior al último como anterior
+        }
+    }
 
     // Buscar un Elemento 
     public boolean buscarElemento() { }
